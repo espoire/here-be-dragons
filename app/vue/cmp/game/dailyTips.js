592281aeb159383d44715bf6386c4c8a1e5d6155
Vivian Lampman <vivian.lampman@gmail.com>
2026-07-31 04:38:07 -0500

    Add a bunch of daily tips

--- app/vue/cmp/game/dailyTips.js
+++ app/vue/cmp/game/dailyTips.js
@@ -3,8 +3,208 @@ export const tips = [
   `
     You are at the 🔻.
     Click adjacent hexes to move.
-    Visiting a hex collects resources.
+    Visiting a hex collects resources. Each hex can only provide resources once per day.
     Moving uses 1 stamina. When stamina runs out or if you return home, the day will end.
-    Return home at the end for a bonus.
+    Return home at the end of the day for a bonus.
+  `,
+  `
+    Your Hero License expires if you adventure for three days without levelling up.
+    Collect 10 XP then return home to the Guild Hall, before the end of your third day.
+  `,
+  `
+    If you have selected a construction to work on, you will automatically give your resources to it when you return home at the end of a day.
+    Gather the right types of resources, return home, and your construction will progress.
+    When it is complete, you will receive a bonus and unlock new constructions.
+  `,
+  `
+    Wood, Stone, Herbs, and Ore are the basic resources.
+    They're all pretty rare, especially Ore!
+    Explore more hexes so you can spot these resources when they appear.
+  `,
+  `
+    Herbs may spread over time, especially if you tend them daily.
+    Wood may also spread, but it grows better if you don't over-harvest it.
+    Ore can only be mined once, and turns into Stone afterwards.
+  `,
+  `
+    Need different resources than what's appearing?
+    Visit a Market. There, you can usually buy the types you need.
+    Bring plenty of Coins, or sell resource types you have in excess.
+  `,
+  `
+    Markets usually sell three of the four basic resource types, but not all of them.
+    Each Market has a different selection, and it stays the same from day to day.
+    Rarer resource types for sale usually have limited stocks.
+  `,
+  `
+    Found a Key? Keys can open treasure Chests, which contain valuable resources and Coins.
+    Each Key can only be used once, and each Chest can only be opened once.
+  `,
+  `
+    Chests sometimes contain useful crafting Blueprints, which you'll need before you can craft anything.
+    Once you have a Blueprint, bring it to an Artisan to learn more!
+  `,
+  `
+    Need Cloth, Leather, Paper, or Metal? These are made by crafting.
+    You can also sometimes find these resources in Chests, or buy them at some Markets.
+  `,
+  `
+    Cloth is crafted from Herbs, which are collected from Herbs and some Forest hexes.
+  `,
+  `
+    Leather is crafted from Hide and Salt.
+    Hide is collected from some defeated Monsters.
+    Salt is sold by most Markets, or with the right upgrades it can be collected from Sea, Mines, and Ore hexes.
+  `,
+  `
+    Paper can be crafted from either Herbs or Wood or Hide. You'll need to find a separate Blueprint for each Paper crafting recipe.
+  `,
+  `
+    Metal is crafted from Ore and Coal. Ore is mined from Ore, Mines, and some Mountain hexes.
+    Coal can be crafted from Wood, mined from Mines hexes, or with the right upgrades it can also be mined from Ore and Stone hexes.
+  `,
+  `
+    Wild monsters are dangerous, especially farther from the Guild Hall.
+    If you are low on HP or challenge a monster too dangerous, your Hero may be Injured or even Die!
+    Injured: lose 2 XP, return home, and skip a few days of adventuring.
+    Die: this Hero is gone, and you'll have to recruit a new one.
+  `,
+  `
+    Low on HP? You can visit a Campsite or an Inn to recover some HP.
+  `,
+  `
+    Eating Food restores HP.
+    You'll need to provide your own Food to cook at a Campsite, but Inns will sell you a hot meal for a Coin or two.
+  `,
+  `
+    Need Food for healing HP?
+    You can collect Food from Forage or Farm hexes, hunt Food from Game or Fishing hexes, or even loot Food from some defeated Monsters.
+    You can also buy Food from a Market or an Inn.
+  `,
+  `
+    Sleeping anywhere restores HP.
+    Better sleep locations restore more HP, and also give you more Stamina the next day.
+  `,
+  `
+    Defeating monsters can be rewarding!
+    They give good XP. Some carry Coins.
+    Most have some other useful resources.
+    Some will drop rare crafting Blueprints.
+  `,
+  `
+    Almost all monsters have valuable monster parts like Bone, Hide, or Slime Cores, which can be sold or crafted-with.
+    Bone is sometime used in Crafting.
+    Hide can be crafted into Leather, which is used in many Constructions and Crafting recipes.
+    Slime Cores are worth a lot of Coins at the market, the villagers use them for waterproofing and alchemy.
+  `,
+  `
+    Defeating a monster removes it from the map.
+    Often after defeating a monster, you may discover a rare or useful map node the next day.
+    Monsters often guard Ore or Chests.
+  `,
+  `
+    Need to journey farther than your stamina allows?
+    You can camp in the wild, but you'll have less Stamina tomorrow.
+    Where possible, rest at a Campsite or better yet, at an Inn.
+  `,
+  `
+    Where you sleep affects how much Stamina you'll have the next day.
+    Sleeping at the Guild Hall or an Inn gives you full Stamina, but the Inn will cost a Coin or two.
+    Sleeping at a Campsite gives you 3 less Stamina.
+    Sleeping in the wild gives you 4 less Stamina, but you'll be able to harvest that tile again in the morning without spending a Stamina to move there.
+  `,
+  `
+    If you visit the same Campsite repeatedly, it will last longer.
+    Disused Campsites will eventually disappear, but don't worry, new ones may appear there or elsewhere in the future.
+  `,
+  `
+    Need to journey farther than your stamina allows?
+    Build the Dormitory and Cobbler Constructions to increase your maximum Stamina.
+    Even better max-Stamina Constructions will require other prerequisite Constructions to be completed first.
+  `,
+  `
+    The same Inn will always charge the same amount of Coins for a meal or a night's stay.
+    Some Inns are more expensive than others, but usually the location is the most important factor.
+    The more Coins you spend at an Inn, the longer it will last!
+  `,
+  `
+    There are Guild Constructions to reduce the Stamina penalty for sleeping in the wild or at a Campsite, and to increase your maximum Stamina.
+  `,
+  `
+    Markets have a fixed duration, after which they will disappear.
+    Nothing you can do will make them last any longer.
+    You can see how many days a Market has left when you visit it.
+    Make sure to buy what you need in time!
+  `,
+  `
+    Villages don't do anything for you directly, but Inns, Markets, and Quests often appear nearby.
+  `,
+  `
+    Monsters are attracted to Villages, and will often appear nearby.
+    A village with multiple undefeated monsters nearby is more likely to vanish.
+  `,
+  `
+    Roads appear where you commonly walk, and make it easier to move around.
+    Travelling on a Road costs only half the usual Stamina.
+    Roads are not permanent, and will eventually disappear if you don't keep using them.
+  `,
+  `
+    Barren tiles are useless... but you might be able to trample one into a Road by visiting it on multiple days in a row.
+  `,
+  `
+    Roads appear more readily on tiles that are adjacent to other Roads, or near Villages.
+  `,
+  `
+    It's rumored that the Dwarves know a method to pave Roads with Stone, so that they last longer.
+    But the Dwarves are a secretive folk, and you may have to earn their trust before they share their secrets.
+  `,
+  `
+    Once the Guild has some Mages, you'll be able to harvest from Leyline hexes.
+    Leylines yield Spelldust or Crystals, which are used in many Constructions and Crafting recipes.
+  `,
+  `
+    Mages can also gain XP or heal HP by visiting a Leyline.
+  `,
+  `
+    A Shrine is a sacred place where you can pray, to gain Faith.
+    If you have Faith, sometime you'll get the option to spend Faith before harvesting a hex, to gain a bonus.
+  `,
+  `
+    Priests have more uses for Faith, and receive more Faith when they visit a Shrine.
+  `,
+  `
+    Faith can neither be sold nor crafted.
+  `,
+  `
+    A new Campsite is more likely to appear at a place if you sleep at that location often.
+  `,
+  `
+    The region southwest of the Guild Hall is a vast forest, and is a good place to find Wood and Herbs.
+    The region east of the Guild Hall is a rocky mountainous area, and is a good place to find Stone and Ore.
+  `,
+  `
+    Dense Forest tiles may contain Herb or Wood... but also might hide a concealed monster. Be careful when exploring these areas!
+    Forests also cost 1.5x Stamina to move through.
+  `,
+  `
+    Mountain tiles may contain Stone or Ore.
+    Mountains also cost 2x Stamina to move through.
+  `,
+  `
+    The same Mountain or Forest tile may contain different resources on different days.
+    You won't know if you'll get anything or what it will be, until you visit.
+  `,
+  `
+    They say there are Elves living deep within the southwest forest.
+    Assuming the stories are true, maybe some will join the Guild if you earn their trust?
+  `,
+  `
+    Sometimes travellers from afar say there's an endless sea far to the north.
+    But that's impossible, right? There's not enough water in the world for such a thing.
+  `,
+  `
+    ...if there WERE a sea, you'd need a boat to explore it.
+    Maybe the Guild could Construct something useful for that.
+    First, you'd need to discover a Sea tile, though.
   `,
 ];
